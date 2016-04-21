dff8305f0408a58b29464219452ab22a19820dad
Daniel Koprowski <Daniel Koprowski>
2016-04-21 19:23:52 +0200

    regex validator works fine

--- plugin.js
+++ plugin.js
@@ -1,9 +1,25 @@
 /* jshint browser: true, jquery: true, devel: true, unused: true */
 (function ( $ ) {
+    // regex
+    $.fn.checkRegex = function(regex) {
+        if(validateRegex(this.val(), regex)){
+            this.addClass("good");
+            this.removeClass("bad");
+        }else{
+            this.addClass("bad");
+            this.removeClass("good");                        
+        }
+        return this;
+    };
+    
+    function validateRegex(text, regex){
+        console.log(regex.test(text));
+        return regex.test(text);
+    };
  
-    var shade = "#556b2f";
- 
-    $.fn.checkEmail = function() {
+ // ----------------------------------
+ // email
+     $.fn.checkEmail = function() {
         if(validateEmail(this.val())){
             this.addClass("good");
         }else{
@@ -15,13 +31,12 @@
     function validateEmail(text){
         return true;
     };
- 
 }( jQuery ));
 
 $(function () {
-    var testInput = $('#testInput');
-    testInput.click(function(){
-       testInput.checkEmail();     
+    var regexInput = $('#regexInput');
+    regexInput.click(function(){
+       regexInput.checkRegex(/[A-Z].+/);     
     });
 
 });
